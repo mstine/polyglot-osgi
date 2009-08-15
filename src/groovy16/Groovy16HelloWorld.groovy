88f5fde7fa0e5d2d118ffe83dbc10d9b8f3b8719
Robert Fischer <robert.fischer@smokejumperit.com>
2009-08-15 15:22:08 -0400

    Shifted to using bnd

---
+++ src/groovy16/Groovy16HelloWorld.groovy
@@ -0,0 +1,11 @@
+package eg.impl.groovy16
+
+import eg.api.*
+
+class Groovy16HelloWorld implements HelloWorld {
+
+	void greet() { 
+		println "Hello, World! (Groovy 1.6)"
+	}
+
+}
--- src/java/eg/impl/java/JavaHelloWorld.java
+++ src/java/eg/impl/java/JavaHelloWorld.java
@@ -5,7 +5,7 @@ import eg.api.*;
 public class JavaHelloWorld implements HelloWorld {
 
 	public void greet() {
-		System.out.println("Hello, World! This is coming from Java!");
+		System.out.println("Hello, World! (Java)");
 	}
 
 }
