67c97b8e9e087715fa33d285ad5ae585b67ca881
Robert Fischer <robert.fischer@smokejumperit.com>
2009-08-15 16:52:46 -0400

    Groovy 1.6 and Groovy 1.7 now run

---
+++ src/groovy17/Groovy17HelloWorld.groovy
@@ -0,0 +1,11 @@
+package eg.impl.groovy17
+
+import eg.api.*
+
+class Groovy17HelloWorld implements HelloWorld {
+
+	void greet() { 
+		println "Hello, World! (Groovy 1.7)"
+	}
+
+}
---
+++ src/groovy17/Groovy17HelloWorldBundle.groovy
@@ -0,0 +1,11 @@
+package eg.impl.groovy17;
+
+import eg.api.*;
+import eg.osgi.helpers.*;
+import org.osgi.framework.*;
+
+class Groovy17HelloWorldBundle extends HelloWorldBundle {
+
+	HelloWorld helloWorld = new Groovy17HelloWorld() 
+
+}
